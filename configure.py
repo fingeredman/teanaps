68af65e91f923fbe2d209381b25815baded2f1e7
Jeon Byeongjin <fingeredman@gmail.com>
2019-08-25 14:18:39 +0900

    Add files via upload

--- configure.py
+++ configure.py
@@ -71,3 +71,16 @@ SKIP_WORD_LIST = ["/"]
 # -----------------------------------------
 # DocumentClustering
 # -----------------------------------------
+def configure_plotly_browser_state():
+    import IPython
+    display(IPython.core.display.HTML('''
+        <script src="/static/components/requirejs/require.js"></script>
+        <script>
+          requirejs.config({
+            paths: {
+              base: '/static/base',
+              plotly: 'https://cdn.plot.ly/plotly-latest.min.js?noext',
+            },
+          });
+        </script>
+        '''))
