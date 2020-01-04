2532d0bfec6668c6924108c94a8f97cf275da196
FINGEREDMAN <fingeredman@gmail.com>
2020-01-05 06:15:07 +0900

    Add files via upload

--- nlp/__init__.py
+++ nlp/__init__.py
@@ -3,5 +3,5 @@ from __future__ import absolute_import
 from teanaps.nlp.Processing import Processing
 from teanaps.nlp.MorphologicalAnalyzer import MorphologicalAnalyzer
 from teanaps.nlp.NamedEntityRecognizer import NamedEntityRecognizer
-from teanaps.nlp.NamedEntityTagger import NamedEntityTagger
+#from teanaps.nlp.NamedEntityTagger import NamedEntityTagger
 from teanaps.nlp.SyntaxAnalyzer import SyntaxAnalyzer
