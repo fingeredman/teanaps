51a32ec6959520d26dfdfec41bfc56e36e7395ad
TEANAPS <fingeredman@gmail.com>
2021-04-10 15:06:35 +0900

    Add files via upload

--- text_analysis/__init__.py
+++ text_analysis/__init__.py
@@ -3,6 +3,6 @@ from __future__ import absolute_import
 from teanaps.text_analysis.CoWordCalculator import CoWordCalculator
 #from teanaps.text_analysis.DocumentSummarizer import DocumentSummarizer
 from teanaps.text_analysis.TfidfCalculator import TfidfCalculator
-from teanaps.text_analysis.TopicClustering import TopicClustering
+#from teanaps.text_analysis.TopicClustering import TopicClustering
 from teanaps.text_analysis.DocumentClustering import DocumentClustering
 from teanaps.text_analysis.SentimentAnalysis import SentimentAnalysis
