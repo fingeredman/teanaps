0926ca19d79b64abb7ab4ba68ab5d9a42e6cd753
TEANAPS <fingeredman@gmail.com>
2021-10-24 02:29:57 +0900

    Add files via upload

--- text_analysis/CoWordCalculator.py
+++ text_analysis/CoWordCalculator.py
@@ -152,7 +152,7 @@ class CoWordCalculator():
         return centrality_dict  
     
     
-    def get_word_network_graph(self, centrality_dict, mode="markers", centrality_th=0.5, ego_node_list=[], node_size_rate=100, edge_width_rate=10):
+    def get_word_network_graph(self, centrality_dict, mode="markers", centrality_th=0.5, ego_node_list=[], node_size_rate=10, edge_width_rate=10, text_size_rate=10):
         tv = TextVisualizer()
         tv.set_plotly()
         '''
@@ -166,7 +166,10 @@ class CoWordCalculator():
         #node_list = [word for word, centrality in centrality_dict.items() if centrality >= centrality_th]
         
         #edge_list = [(a, b, w) for (a, b), w in self.get_edge_list() if a in node_list and b in node_list and (a in ego_node_list or b in ego_node_list)]
-        edge_list = [(a, b, w) for (a, b), w in self.get_edge_list() if w >= centrality_th and (a in ego_node_list or b in ego_node_list)]
+        if len(ego_node_list) > 0:
+            edge_list = [(a, b, w) for (a, b), w in self.get_edge_list() if w >= centrality_th and (a in ego_node_list or b in ego_node_list)]
+        else:
+            edge_list = [(a, b, w) for (a, b), w in self.get_edge_list() if w >= centrality_th]
         node_list = []
         for a, b, c in edge_list:
             #if c >= centrality_th:
