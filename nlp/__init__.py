a7ec5aeba7c93af7bc1f27a6ff53a1850309a187
TEANAPS <fingeredman@gmail.com>
2023-06-24 15:05:13 +0900

    Update __init__.py

--- nlp/__init__.py
+++ nlp/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 
-#from teanaps.nlp.NamedEntityRecognizer import NamedEntityRecognizer
-#from teanaps.nlp.NamedEntityTagger import NamedEntityTagger
-#from teanaps.nlp.SyntaxAnalyzer import SyntaxAnalyzer
+from teanaps.nlp.NamedEntityRecognizer import NamedEntityRecognizer
+from teanaps.nlp.NamedEntityTagger import NamedEntityTagger
+from teanaps.nlp.SyntaxAnalyzer import SyntaxAnalyzer
 from teanaps.nlp.Processing import Processing
 from teanaps.nlp.MorphologicalAnalyzer import MorphologicalAnalyzer
